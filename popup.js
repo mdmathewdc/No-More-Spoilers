10e2cb6901ea1cd537458c102142f8f38111390f
Mathew Dony Chittezhath <mathewdony007@gmail.com>
2021-12-20 23:24:46 +1100

    Added navbar

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+// let changeColor = document.getElementById("changeColor");
 let inputBox = document.getElementById("textbox");
 let addButton = document.getElementById("add-button");
 var spoilerTerms = [];
@@ -9,22 +9,22 @@ chrome.storage.sync.get("color", ({ color }) => {
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// changeColor.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       function: setPageBackgroundColor,
+//     });
+//   });
   
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
+//   // The body of this function will be executed as a content script inside the
+//   // current page
+//   function setPageBackgroundColor() {
+//     chrome.storage.sync.get("color", ({ color }) => {
+//       document.body.style.backgroundColor = color;
+//     });
+//   }
 
   // Remove disabled property from 'Add' button when text input box is not empty
   inputBox.addEventListener("input", () => {
