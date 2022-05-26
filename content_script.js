c64e570212aefc1beb342eaab00f7090314e03e7
Mathew Dony <mathewdony007@gmail.com>
2022-05-26 18:35:42 +1000

    Add spoiler style

--- content_script.js
+++ content_script.js
@@ -1,1 +1,11 @@
-document.body.style.backgroundColor = "blue";
+const text = 'cachedTerms';
+
+const matches = [];
+
+for (const span of document.querySelectorAll('span')) {
+  if (span.textContent.includes(text)) {
+    matches.push(span);
+    // alert(span.textContent);
+    span.className += ' hide-spoiler';
+  }
+}
